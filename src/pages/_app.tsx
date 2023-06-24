fcfd104eb5e0cc6bea289b164b1a55ea3c38754a
u-ecmaker <u.ecmaker@gmail.com>
2023-06-24 16:25:46 +0900

    #21 google analyticsを導入 test
    ## 参考
    - Notion Blog に Google Analytics を追加する (oikawa.dev)
    
    ## 残件
    - pv数がただしく表示されるかの確認

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,26 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import "@/styles/globals.css";
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Header />
       <main>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        />
+       <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GA_TRACKING_ID}');
+            `,
+          }}
+        />
         <Component {...pageProps} />
       </main>
       <Footer />
