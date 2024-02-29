ee022cce0d197e257d0c7d7f295674a3c9a01121
u-ecmaker <u.ecmaker@gmail.com>
2024-02-29 14:52:38 +0900

    かなるs update next/image の ドメイン追加： #19 表示できる。

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     domains: [
       "s3.us-west-2.amazonaws.com",
       "picsum.photos",
+      "amazonaws.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com"
     ],
     formats: ["image/avif", "image/webp"],
   },
