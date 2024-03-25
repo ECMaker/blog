451089706c1a47ff995bc3b8e019eb777e3d6c56
u-ecmaker <u.ecmaker@gmail.com>
2024-03-26 08:46:10 +0900

    bug/#38 ctr+k algolia search Return URL: fix.
    - 一個前のコミットで解消されていた。Algoliaのページで確認可能
      https://dashboard.algolia.com/apps/S8EVF44K6U/indices
    - indexSearchでIndexの設定
      AlgoliaのページのIndicesのフォルダ分けみたいなもの。

--- src/server/utils/algolia.ts
+++ src/server/utils/algolia.ts
@@ -7,7 +7,7 @@ const client = algoliasearch(
   `${process.env.ALGOLIA_APPLICATION_ID}`,
   `${process.env.ALGOLIA_ADMIN_API_KEY}`
 );
-const index = client.initIndex('noblog');
+const index = client.initIndex('ECmaker');
 
 export const saveToAlgolia = async (post: NotionPost) => {
   try {
