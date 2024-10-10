b5b3ed6d757f9320ca0dce5537bde751a9c078a9
u-ecmaker <u.ecmaker@gmail.com>
2024-10-10 19:09:55 +0900

    PR #58 #73 and heading3 bug/ #58 #73 ToCs. run dev, build OK.
    Merge pull request #66 from ECMaker:bug/#58_H1TableOfContents
    bug #58 fix
    
    Merge pull request #74 from ECMaker:bug/#73_ToClinks
    close #73 bug/#73 ToCのH1リンクが機能していない fix
    
    Merge branch 'style/heading3' (67eb9d8)
    
    cb310ff で対処した src\recoil\atoms.ts の不明なエラーは関係なかったのでrevertした。
    またblock.idが絡んでそうで根が深いかも。

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from 'recoil';
 
 export const inViewHeadingIdsAtom = atom<string[]>({
-  key: 'uniqueInViewHeadingAtom', // キーを一意に変更
+  key: 'inViewHeadingAtom',
   default: [],
 });
