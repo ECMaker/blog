364bbe94718ff4a0c9300a56c5e0ef2c9ae1d0b9
u-ecmaker <u.ecmaker@gmail.com>
2024-10-10 22:41:31 +0900

    PR #67 bug/ #38 ctr+kが働かない. run dev, build OK.

--- src/server/utils/algolia.ts
+++ src/server/utils/algolia.ts
@@ -7,7 +7,7 @@ const client = algoliasearch(
   `${process.env.ALGOLIA_APPLICATION_ID}`,
   `${process.env.ALGOLIA_ADMIN_API_KEY}`,
 );
-const index = client.initIndex('noblog');
+const index = client.initIndex('ECmaker');
 
 export const saveToAlgolia = async (post: NotionPost) => {
   try {
@@ -23,7 +23,7 @@ export const saveToAlgolia = async (post: NotionPost) => {
       .join('');
 
     const records: AlgoliaSearchObjectRequest = {
-      objectID: post.id,
+      objectID: post.slug,
       title: post.title,
       category: post.category.name,
       tags: post.tags.map((tag) => tag.name),
