3af760a7d4d495c47f98172dd3db1e010dba056e
u-ecmaker <u.ecmaker@gmail.com>
2024-10-10 18:25:22 +0900

    PR #65 bug/ #62 bookmark. run dev, build OK.

--- src/server/utils/ogp.ts
+++ src/server/utils/ogp.ts
@@ -5,7 +5,6 @@ import type { Ogp } from '~/types/ogp';
 import ogpParser from 'ogp-parser';
 
 /* OGPを取得する（Node.jsで使用を想定） */
-
 export const getOgp = async (url: string): Promise<Ogp> => {
   try {
     const encodeURL = encodeURI(url);
@@ -33,23 +32,39 @@ export const getOgp = async (url: string): Promise<Ogp> => {
   }
 };
 
-/* NotionBlockObjectのBookmarkにOPG情報を差し込む */
+/* ブックマークデータを再帰的に検索し、各ブックマークに OGP を設定する */
+const findeBookmark = async (
+  data: ExpandedBlockObjectResponse | ExpandedBlockObjectResponse[],
+): Promise<ExpandedBlockObjectResponse | ExpandedBlockObjectResponse[]> => {
+  if (Array.isArray(data))
+    return (await Promise.all(
+      data.map(findeBookmark),
+    )) as ExpandedBlockObjectResponse[];
+  if (typeof data !== 'object' || data === null) return data;
+  for (const key in data) {
+    if (data.type !== 'bookmark') {
+      (data as any)[key] = await findeBookmark((data as any)[key]);
+      continue;
+    }
+    const url = (
+      data as ExpandedBlockObjectResponse & { bookmark: { url: string } }
+    ).bookmark.url;
+    const ogp = await getOgp(url);
+
+    return {
+      ...data,
+      ogp,
+    } as BookmarkBlockObjectResponse & { ogp: Ogp };
+  }
+
+  return data;
+};
+
+/* NotionBlockObjectのBookmarkにOGP情報を差し込む */
 export const setOgp = async (
   children: ExpandedBlockObjectResponse[],
 ): Promise<ExpandedBlockObjectResponse[]> => {
-  const results = await Promise.all(
-    children.map(async (child) => {
-      if (child.type !== 'bookmark') return child;
-
-      const url = child.bookmark.url;
-      const ogp = await getOgp(url);
-
-      return {
-        ...child,
-        ogp,
-      } as BookmarkBlockObjectResponse & { ogp: Ogp };
-    }),
-  );
-
-  return results;
+  return (await Promise.all(
+    children.map(findeBookmark),
+  )) as ExpandedBlockObjectResponse[];
 };
