5d33fbd9883ea7dfdded7c49ed108bcef6d4da4b
u-ecmaker <u.ecmaker@gmail.com>
2024-10-12 20:46:36 +0900

    bug/ #77 F12 撲滅: copy cb310ff ブロックfix.
     cb310ff のcherry-pick.
    fix.
    child.id と block.id で競合、重複しているbugに対処した。
    Bug/#32_箇条書きのネスト, Feature/#59_未対応ブロックへの対応

--- src/components/notion/blocks/BulletedList.tsx
+++ src/components/notion/blocks/BulletedList.tsx
@@ -10,7 +10,7 @@ export const BulletedList = ({ block }: Props) => {
   return (
     <ul className="my-5 list-disc pl-7">
       {block.bulleted_list.children.map((child) => (
-        <div key={block.id}>{blockToJsx(child)}</div>
+        <div key={child.id}>{blockToJsx(child)}</div>
       ))}
     </ul>
   );
