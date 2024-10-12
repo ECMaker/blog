401f4194e2544c38e883dfbf0dd7026d129cb33f
u-ecmaker <u.ecmaker@gmail.com>
2024-10-13 07:41:42 +0900

    bug/ #77 F12 撲滅: run dev,build ok.

--- src/components/@layouts/SearchButton.tsx
+++ src/components/@layouts/SearchButton.tsx
@@ -1,19 +1,23 @@
 import type { FC } from 'react';
 
 import { Kbd } from '@mantine/core';
-import { useOs } from '@mantine/hooks';
-import { useSpotlight } from '@mantine/spotlight';
 
 import { SearchIcon } from '~/commons/icons';
 
-export const SearchButton: FC = () => {
-  const spotlight = useSpotlight();
-  const os = useOs();
+type Props = {
+  onClick: () => void;
+};
+export const SearchButton: FC<Props> = ({ onClick }) => {
+
+  const isMac = typeof window !== 'undefined' && navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+
+  const keyLabel = isMac ? '⌘' : 'Ctrl';
 
   return (
     <button
       className="flex cursor-pointer items-center gap-2 sp:gap-1 rounded-md border-none bg-gray-700 px-2 py-1 md:py-1.5 sp:pr-5 sm:pr-5 md:pr-1 font-Baloo text-base text-white shadow hover:brightness-125"
-      onClick={() => spotlight.openSpotlight()}
+      tabIndex={0}
+      onClick={onClick}
     >
       <SearchIcon size={18} />
       Search
@@ -21,7 +25,7 @@ export const SearchButton: FC = () => {
         className="border-gray-400 bg-gray-700 py-0.5 text-xs text-white hidden md:block"
         color="dark"
       >
-        {os === 'windows' ? 'Ctrl' : '⌘'} + K
+        {keyLabel} + K
       </Kbd>
     </button>
   );
