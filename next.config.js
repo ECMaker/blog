b8326fb7d17d63048efb35baaf7a76d83bf002cd
u-ecmaker <u.ecmaker@gmail.com>
2024-10-13 12:02:16 +0900

    bug/#77. actions, next config update

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ const nextConfig = {
 };
 
 module.exports = {
+  ...nextConfig,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -26,5 +27,4 @@ module.exports = {
 
     return config;
   },
-  ...nextConfig,
 };
