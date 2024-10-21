104d9b5148ec1a547245a341688c22b0ea5f81da
u-ecmaker <u.ecmaker@gmail.com>
2024-10-21 15:34:22 +0900

    fix/#91 各NotionBlockへの細かい対応 #53 image続き 縦横比 fix
    - 縦横比から画像サイズを動的に変更
    - センター構えに変更

--- src/components/notion/blocks/Image/Image.tsx
+++ src/components/notion/blocks/Image/Image.tsx
@@ -21,15 +21,19 @@ export const Image: FC<Props> = ({ block }: Props) => {
       ? block.image.external.url
       : block.image.file.url;
   const caption =
-   block.image.caption.length > 0 ? block.image.caption[0].plain_text : '';
+    block.image.caption.length > 0 ? block.image.caption[0].plain_text : '';
   block.image.caption ? richTextToString(block.image.caption) : '';
   const [isError, setIsError] = useState(false);
 
+  // !U 画像の縦横比を管理するための状態を定義 画像の縦横比に基づいて適切なクラス名を決定する
+  const [aspectRatio, setAspectRatio] = useState<number | null>(null);     
+  const imageClassName = aspectRatio && aspectRatio > 1 ? "object-contain h-auto w-full" : "object-contain h-auto w-auto";
+
   return (
-    <div className="relative mx-auto">
+    <div className="relative mx-auto mt-4 mb-4 flex flex-col items-center">
       <MediumZoom zoomMargin={40}>
           <NextImage
-            className="object-contain h-auto w-full"
+            className={imageClassName} // !U className を動的に変更
             src={url}
             alt={caption || ''}
             width={3840}
@@ -40,35 +44,41 @@ export const Image: FC<Props> = ({ block }: Props) => {
               console.error(e);
               setIsError(true);
             }}
+            // !U 画像の読み込み完了時に縦横比を計算
+            onLoadingComplete={({naturalWidth, naturalHeight}) => {
+              setAspectRatio(naturalWidth / naturalHeight);
+            }}
           />
         </MediumZoom>
       {caption && (
-        <figcaption className="text-xs text-gray-400">{caption}</figcaption>
+        <figcaption className="text-xs text-gray-400 text-center">
+          {caption}
+        </figcaption>
       )}
-        {isError && (
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="text-center">
-              <div
-                className="mx-auto mt-1 w-60 text-ellipsis text-sm line-clamp-1"
-                title={url}
+      {isError && (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="text-center">
+            <div
+              className="mx-auto mt-1 w-60 text-ellipsis text-sm line-clamp-1"
+              title={url}
+            >
+              {url}
+            </div>
+            <div className="item-center mt-2 flex gap-2 text-2xl font-bold">
+              <DangerIcon size={36} />
+              画像の取得に失敗しました 🥺
+            </div>
+            <div className="mt-3">
+              <Button
+                rightIcon={<UpdateIcon size={20} />}
+                onClick={() => window.location.reload()}
               >
-                {url}
-              </div>
-              <div className="item-center mt-2 flex gap-2 text-2xl font-bold">
-                <DangerIcon size={36} />
-                画像の取得に失敗しました 🥺
-              </div>
-              <div className="mt-3">
-                <Button
-                  rightIcon={<UpdateIcon size={20} />}
-                  onClick={() => window.location.reload()}
-                >
-                  更新する
-                </Button>
-              </div>
+                更新する
+              </Button>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
+    </div>
   );
 };
