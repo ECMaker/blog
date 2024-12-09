b149b344bc8d07737981e944a4e0a8e8704f5c56
u-ecmaker <u.ecmaker@gmail.com>
2024-12-09 17:07:29 +0900

    feature/#99_画像url自動更新  test AudioSetState
    - test: audioにuseStateを追加。
      - loading中によりどうなるか。。

--- src/components/notion/blocks/Audio.tsx
+++ src/components/notion/blocks/Audio.tsx
@@ -1,22 +1,30 @@
 import type { AudioBlockObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 import type { BlockWithChildren } from '~/types/notion';
 
+import { useState, useEffect } from 'react';
+
 type Props = {
   block: BlockWithChildren<AudioBlockObjectResponse>;
 };
 
 export const Audio = ({ block: { audio } }: Props) => {
   const caption = audio.caption.length > 0 ? audio.caption[0].plain_text : '';
+  const [audioUrl, setAudioUrl] = useState(audio.type === 'file' ? audio.file.url : audio.external.url);
+
+  useEffect(() => {
+    setAudioUrl(audio.type === 'file' ? audio.file.url : audio.external.url);
+  }, [audio]);
+
 
   return (
     <div className="my-4 flex flex-col items-center">
       <audio controls className="w-full">
         <source
-          src={audio.type === 'file' ? audio.file.url : audio.external.url}
+          src={audioUrl}
           type="audio/ogg"
         />
         <source
-          src={audio.type === 'file' ? audio.file.url : audio.external.url}
+          src={audioUrl}
           type="audio/mpeg"
         />
         Your browser does not support the audio element.
